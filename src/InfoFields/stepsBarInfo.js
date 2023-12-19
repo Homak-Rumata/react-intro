35cde4346b7db318734e17740586bf4bf8429032
Nikita Nikita <repnikov-nik@mail.ru>
2023-12-19 08:01:02 +0700

    stepsbarinfo

--- src/InfoFields/stepsBarInfo.js
+++ src/InfoFields/stepsBarInfo.js
@@ -1,8 +1,8 @@
 function StepsBarInfo () {
     return [{text: "Личные данные", number: 1},
             {text: "Особые права", number: 2},
-            {text: "Профильная организация", number: 3}, 
-            {text: "Руководители практики", number: 4},
+            {text: "Руководители практики", number: 3}, 
+            {text: "Профильная организация", number: 4},
             {text: "Печать документа", number: 5}]
 }
 
